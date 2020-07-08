6f7a1de59e7b7d12d83f86c121a03d88d8b54800
Marcio Maciel <marcio.maciel@evino.com.br>
2020-07-07 21:36:11 -0300

    Add magento host to hosts file

--- scripts/api-functional.sh
+++ scripts/api-functional.sh
@@ -2,5 +2,6 @@
 
 set -e
 
+echo '127.0.0.1 magento.localhost' >> /etc/hosts
 cd $PROJECT_DIR/dev/tests/api-functional
 ../../../vendor/bin/phpunit -c $PROJECT_DIR/dev/tests/api-functional/phpunit_graphql.xml
