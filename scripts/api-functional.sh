ceea9db4d6d8a591ef8f23aa6ba863a6f69594b2
Marcio Maciel <marcio.maciel@evino.com.br>
2020-07-07 21:13:08 -0300

    Fix phpunit config path

--- scripts/api-functional.sh
+++ scripts/api-functional.sh
@@ -3,4 +3,4 @@
 set -e
 
 cd $PROJECT_DIR/dev/tests/api-functional
-../../../vendor/bin/phpunit -c /var/www/html/dev/tests/api-functional/phpunit_graphql.xml
+../../../vendor/bin/phpunit -c $PROJECT_DIR/dev/tests/api-functional/phpunit_graphql.xml
